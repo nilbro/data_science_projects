0dc2d3e14fa3a8d391ec6a3fd25198cb3945fdd0
nilbro <nlbhrptr@hotmail.com>
2020-01-11 12:26:26 +0100

    connect and create tables

--- data_engineering/data_modelling/create_tables.py
+++ data_engineering/data_modelling/create_tables.py
@@ -1,4 +1,5 @@
 import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
 
 def create_database():
     # connect to default database
@@ -19,8 +20,28 @@ def create_database():
 
     return cur, conn
 
+def drop_tables(cur,conn):
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+def create_tables(cur,conn):
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
 def main():
     cur, conn = create_database()
+
+    # drop tables if they exist
+    drop_tables(cur,conn)
+
+    # create tables if they exist
+    create_tables(cur,conn)
+
+    # close the connection
+    conn.close()
+
 
 if __name__ == "__main__":
     main()
